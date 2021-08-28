cad9aa0034cb44ceac3d510b969589310bdc45cd
nihaltowfiq <nihaltowfiq@gmail.com>
2021-08-29 00:46:43 +0600

    data updated

--- src/libs/database/destinationData.js
+++ src/libs/database/destinationData.js
@@ -1,29 +1,29 @@
 export const destinationData = [
 	{
 		id: '01',
-		name: 'Sajek',
+		name: 'Sajek Valley',
 		shortDescription:
-			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, exercitationem dolorem vel dignissimos quibusdam soluta facere? Illo dolores molestiae dicta?',
+			'Sajek Valley is an emerging tourist spot in Bangladesh situated among the hills of the Kasalong range of mountains in Sajek union, Baghaichhari Upazila in the Rangamati District. The valley is 1,476 feet (450 m) above sea level. Sajek valley is known as the Queen of Hills & Roof of Rangamati.',
 		img: '/images/sajek.png',
 		longDescription:
-			'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci eaque maiores quam repudiandae minus impedit quisquam sint quibusdam, quod possimus totam eius voluptatibus corrupti saepe. Quod saepe nemo, et harum voluptate possimus neque doloribus repellat ipsam tenetur dolorum necessitatibus est? Illum eius repellat quisquam minus expedita iure odit dolor dolores?',
+			'Sajek Valley is an emerging tourist spot in Bangladesh situated among the hills of the Kasalong range of mountains in Sajek union, Baghaichhari Upazila in the Rangamati District. The valley is 1,476 feet (450 m) above sea level. Sajek valley is known as the Queen of Hills & Roof of Rangamati. Sajek is located in the verdant hills of Kasalong range of mountains amidst the serene and exotic beauty of nature. Lofty mountains, dense forest, sprawling grasslands and miles of hilly tracks feature the valley. The simple and basic indigenous lifestyle of local people is just fabulous for visitors to spend a day.',
 	},
 	{
 		id: '02',
 		name: 'Sreemongol',
 		shortDescription:
-			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, exercitationem dolorem vel dignissimos quibusdam soluta facere?',
+			'Sreemangal is a hilly area covered with tea estates. There are 47 tea gardens in Sreemangal. A large portion of world’s highest quality tea is grown here. It is also called the city of ‘two leaves and a bud. Sreemangal is famous for nature, forests and wildlife.',
 		img: '/images/sreemongol.png',
 		longDescription:
-			'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci eaque maiores quam repudiandae minus impedit quisquam sint quibusdam, quod possimus totam eius voluptatibus corrupti saepe. Quod saepe nemo, et harum voluptate possimus neque doloribus repellat ipsam tenetur dolorum necessitatibus est? Illum eius repellat quisquam minus expedita iure odit dolor dolores?',
+			'Sreemangal, The Tea Capital of Bangladesh.	Sreemangal is a hilly area covered with tea estates. There are 47 tea gardens in Sreemangal. A large portion of world’s highest quality tea is grown here. It is also called the city of ‘two leaves and a bud. Sreemangal is famous for nature, forests and wildlife. This little town is located about 200 KM northeast of Dhaka. Once Sreemangal was a part of the Tripura State. The great earthquake of 1897 caused a massive change in the physiography of the area. Main attraction are the Lawachara National Park (tropical rain forest), Madhabpur lake (the lake of lotus), Tea gardens, Monipuri & Khashia tribal villages, Baikkabeel (wetland of seasonal birds) and also the	world famous 7 layer-color tea cabin.',
 	},
 	{
 		id: '03',
-		name: 'Sundarban',
+		name: 'Sundarbans',
 		shortDescription:
-			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, exercitationem dolorem vel dignissimos quibusdam soluta facere? Illo dolores molestiae dicta? ipsum dolor sit amet, consectetur adipisicing elit.',
+			'Sundarbans is a mangrove area in the delta formed by the confluence of the Ganges, Brahmaputra and Meghna Rivers in the Bay of Bengal. It spans from the Hooghly River in India’s state of West Bengal to the Baleswar River in Bangladesh’s division of Khulna.',
 		img: '/images/sundarban.png',
 		longDescription:
-			'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci eaque maiores quam repudiandae minus impedit quisquam sint quibusdam, quod possimus totam eius voluptatibus corrupti saepe. Quod saepe nemo, et harum voluptate possimus neque doloribus repellat ipsam tenetur dolorum necessitatibus est? Illum eius repellat quisquam minus expedita iure odit dolor dolores?',
+			'The Sundarbans mangrove forest, one of the largest such forests in the world (140,000 ha), lies on the delta of the Ganges, Brahmaputra and Meghna rivers on the Bay of Bengal. It is adjacent to the border of India’s Sundarbans World Heritage site inscribed in 1987. The site is intersected by a complex network of tidal waterways, mudflats and small islands of salt-tolerant mangrove forests, and presents an excellent example of ongoing ecological processes. The area is known for its wide range of fauna, including 260 bird species, the Bengal tiger and other threatened species such as the estuarine crocodile and the Indian python.',
 	},
 ];
